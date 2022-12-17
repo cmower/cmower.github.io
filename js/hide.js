236f560f8bfeceb2ebd96fbe578e41ff513009bf
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-12-17 18:16:35 +0000

    Show more/less

--- js/hide.js
+++ js/hide.js
@@ -1,9 +1,12 @@
-function toggleblock(blockId)
+function toggleblock(blockId, selfId)
 {
-   var block = document.getElementById(blockId);
-   if (block.style.display == 'none') {
-    block.style.display = 'block' ;
-   } else {
-    block.style.display = 'none' ;
-   }
+    var block = document.getElementById(blockId);
+    var sblock = document.getElementById(selfId);
+    if (block.style.display == 'none') {
+	block.style.display = 'block' ;
+	sblock.textContent = '---- show less ----';
+    } else {
+	block.style.display = 'none' ;
+	sblock.textContent = '---- show more ----';
+    }
 }
