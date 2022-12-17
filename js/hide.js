2f071d2a08271c60fbfd17fc458f8866ddb36eaf
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-12-17 16:57:53 +0000

    Add ability to toggle sections and add news item re corl22

---
+++ js/hide.js
@@ -0,0 +1,9 @@
+function toggleblock(blockId)
+{
+   var block = document.getElementById(blockId);
+   if (block.style.display == 'none') {
+    block.style.display = 'block' ;
+   } else {
+    block.style.display = 'none' ;
+   }
+}
