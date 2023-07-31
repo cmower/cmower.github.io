42f92c310bcd8b70d907070b4ee28281119441a5
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-07-31 11:03:17 +0100

    Several updates

--- js/hide.js
+++ js/hide.js
@@ -10,3 +10,16 @@ function toggleblock(blockId, selfId)
 	sblock.textContent = '---- show more ----';
     }
 }
+
+function togglebioblock(blockId, selfId)
+{
+    var block = document.getElementById(blockId);
+    var sblock = document.getElementById(selfId);
+    if (block.style.display == 'none') {
+	block.style.display = 'block' ;
+	sblock.textContent = '---- read less (long bio) ----';
+    } else {
+	block.style.display = 'none' ;
+	sblock.textContent = '---- read more (long bio) ----';
+    }
+}
